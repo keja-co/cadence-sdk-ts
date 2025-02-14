14363ba61edd72c0d656ad69a0d93f83a96d1eba
Myles K <11828103+myleskeeffe@users.noreply.github.com>
2025-02-14 10:38:29 +0800

    Tidy up repo

--- apis/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ComplianceApi';
-export * from './HrApi';
-export * from './IdentityApi';
-export * from './LocationApi';
-export * from './SchemaApi';
